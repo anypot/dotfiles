5508ca7c7cd55dc650f22b40b02cf86470b62583
Anthony 'anypot' Poinsot <anypot@posteo.net>
2017-11-09 16:38:27 +0100

    Correctif des scripts suite aux changements de nom

--- my_scripts/Scripts/backup_pc.sh
+++ my_scripts/Scripts/backup_pc.sh
@@ -4,6 +4,6 @@ BACKUP_DIR="Backup"
 SCRIPTS_DIR="Scripts"
 DOTFILES_DIR="dotfiles"
 
-sh $HOME/$SCRIPTS_DIR/savepaclist.sh
+sh $HOME/$SCRIPTS_DIR/save_pacman_list.sh
 tar -cjf $HOME/$BACKUP_DIR/$(hostname)-backup.tar.bz2 -C $HOME $BACKUP_DIR/pkg.list $BACKUP_DIR/aur.list $DOTFILES_DIR
 
--- my_scripts/Scripts/save_dirs.sh
+++ my_scripts/Scripts/save_dirs.sh
@@ -6,5 +6,5 @@ BACKUP_DIR="$HOME/Backup"
 SCRIPTS_DIR="$HOME/Scripts"
 
 tar -cjf $BACKUP_DIR/etc-backup.tar.bz2 /etc
-tar -cjf $BACKUP_DIR/home-backup.tar.bz2 -X $SCRIPTS_DIR/excludesavehome.txt $HOME
+tar -cjf $BACKUP_DIR/home-backup.tar.bz2 -X $SCRIPTS_DIR/exclude_save_home_dir.txt $HOME
 tar -cjf $BACKUP_DIR/pacman-database.tar.bz2 /var/lib/pacman/local
