4e1943d54de804852301dd1dada1cc12c52db2fe
Anthony 'anypot' Poinsot <anypot@posteo.net>
2021-04-16 22:57:36 +0200

    Add sxiv initial config

--- desktop/zsh/.oh-my-zsh/custom/config.zsh
+++ desktop/zsh/.oh-my-zsh/custom/config.zsh
@@ -5,11 +5,15 @@ export GPG_TTY=$TTY
 export EDITOR="nvim"
 export PAGER="less"
 export BROWSER="firefox"
+export PICVIEW="sxiv"
+export SNDPLAY="mpv"
 export MOVPLAY="mpv"
 export TERMINAL="alacritty"
 # For ranger :terminal command
 export TERMCMD="alacritty"
 
-for ext in com html net no org php pdf; do alias -s $ext=$BROWSER; done
-for ext in PKGBUILD py tex txt;         do alias -s $ext=$EDITOR; done
-for ext in avi mkv mp4 mpg wmv;         do alias -s $ext=$MOVPLAY; done
+for ext in com html net no org php pdf;              do alias -s $ext=$BROWSER; done
+for ext in md PKGBUILD py tex txt;                   do alias -s $ext=$EDITOR; done
+for ext in bmp cdr gif ico jpeg jpg JPG png svg xpm; do alias -s $ext=$PICVIEW; done
+for ext in flac mp3 ogg wav;                         do alias -s $ext=$SNDPLAY; done
+for ext in avi mkv mp4 mpg wmv;                      do alias -s $ext=$MOVPLAY; done
