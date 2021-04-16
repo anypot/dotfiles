3c0f8ae590ad33efd470440fe5248d20ad5a4bbf
Anthony 'anypot' Poinsot <anypot@posteo.net>
2021-04-16 22:12:29 +0200

    Add mpv config

--- desktop/zsh/.oh-my-zsh/custom/config.zsh
+++ desktop/zsh/.oh-my-zsh/custom/config.zsh
@@ -5,9 +5,11 @@ export GPG_TTY=$TTY
 export EDITOR="nvim"
 export PAGER="less"
 export BROWSER="firefox"
+export MOVPLAY="mpv"
 export TERMINAL="alacritty"
 # For ranger :terminal command
 export TERMCMD="alacritty"
 
 for ext in com html net no org php pdf; do alias -s $ext=$BROWSER; done
-for ext in PKGBUILD py tex txt;	        do alias -s $ext=$EDITOR; done
+for ext in PKGBUILD py tex txt;         do alias -s $ext=$EDITOR; done
+for ext in avi mkv mp4 mpg wmv;         do alias -s $ext=$MOVPLAY; done
