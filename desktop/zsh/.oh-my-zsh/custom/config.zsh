b670b9bfb0bd97580a16574edca3cec70edf9ad8
Anthony 'anypot' Poinsot <anypot@posteo.net>
2021-04-16 20:41:37 +0200

    Some default programs

--- desktop/zsh/.oh-my-zsh/custom/config.zsh
+++ desktop/zsh/.oh-my-zsh/custom/config.zsh
@@ -1,2 +1,13 @@
 # To fix the error: gpg failed to sign the data
 export GPG_TTY=$TTY
+
+# Default Programs
+export EDITOR="nvim"
+export PAGER="less"
+export BROWSER="firefox"
+export TERMINAL="alacritty"
+# For ranger :terminal command
+export TERMCMD="alacritty"
+
+for ext in com html net no org php pdf; do alias -s $ext=$BROWSER; done
+for ext in PKGBUILD py tex txt;	        do alias -s $ext=$EDITOR; done
