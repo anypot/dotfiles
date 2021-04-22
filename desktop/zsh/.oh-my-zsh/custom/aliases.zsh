8c0a388854f865fada6a31b207ff536cea3f204a
Anthony 'anypot' Poinsot <anypot@posteo.net>
2021-04-22 20:22:39 +0200

    New alacritty, streamlink and gpg-agent configs

--- desktop/zsh/.oh-my-zsh/custom/aliases.zsh
+++ desktop/zsh/.oh-my-zsh/custom/aliases.zsh
@@ -4,3 +4,16 @@ alias vi='nvim'
 alias diff='nvim -d'
 alias shutdown='sudo shutdown -h now'
 alias reboot='sudo reboot'
+
+# exa
+alias ls='exa'                                                         # ls
+alias l='exa -lbF --git'                                               # list, size, type, git
+alias ll='exa -lbGF --git'                                             # long list
+alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias lS='exa -1'			                                                  # one column, just names
+alias lt='exa --tree --level=2'                                         # tree
+
+# Misc
+alias weather='curl wttr.in'
--- desktop/zsh/.oh-my-zsh/custom/config.zsh
+++ desktop/zsh/.oh-my-zsh/custom/config.zsh
@@ -2,9 +2,11 @@
 export GPG_TTY=$TTY
 
 # Default Programs
+export VISUAL="nvim"
 export EDITOR="nvim"
 export PAGER="less"
 export BROWSER="firefox"
+export READER="zathura"
 export PICVIEW="sxiv"
 export SNDPLAY="mpv"
 export MOVPLAY="mpv"
@@ -12,8 +14,9 @@ export TERMINAL="alacritty"
 # For ranger :terminal command
 export TERMCMD="alacritty"
 
-for ext in com html net no org php pdf;              do alias -s $ext=$BROWSER; done
 for ext in md PKGBUILD py tex txt;                   do alias -s $ext=$EDITOR; done
+for ext in com html net no org php;                  do alias -s $ext=$BROWSER; done
+for ext in pdf;                                      do alias -s $ext=$READER; done
 for ext in bmp cdr gif ico jpeg jpg JPG png svg xpm; do alias -s $ext=$PICVIEW; done
 for ext in flac mp3 ogg wav;                         do alias -s $ext=$SNDPLAY; done
 for ext in avi mkv mp4 mpg wmv;                      do alias -s $ext=$MOVPLAY; done
