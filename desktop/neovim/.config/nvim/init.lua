c970dca20116dfae07eb5144762ee6a7ab8770b6
Anthony 'anypot' Poinsot <anypot@posteo.net>
2021-07-26 20:17:20 +0200

    Gitsign setup

--- desktop/neovim/.config/nvim/init.lua
+++ desktop/neovim/.config/nvim/init.lua
@@ -37,7 +37,7 @@ end)
 
 -- Set colorscheme
 vim.o.termguicolors = true
---vim.g.tokyonight_style = "night" -- default = storm
+vim.g.tokyonight_style = "night" -- default = storm
 vim.g.tokyonight_sidebars = { "packer" }
 vim.cmd[[colorscheme tokyonight]]
 
@@ -66,15 +66,42 @@ vim.api.nvim_exec(
 )
 
 -- Gitsigns
-require('gitsigns').setup()
+require('gitsigns').setup {
+  signs = {
+    add = { text = '▋' },
+    change = { text = '▋' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '▋' },
+  },
+  keymaps = {
+    -- Default keymap options
+    noremap = true,
+
+    ['n ]h'] = { expr = true, "&diff ? ']h' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'"},
+    ['n [h'] = { expr = true, "&diff ? '[h' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'"},
+
+    ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
+    ['v <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
+    ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
+    ['v <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
+    ['n <leader>hR'] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
+    ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
+    ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line(true)<CR>',
+
+    -- Text objects
+    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
+    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
+  }
+}
 
 -- Indent-blankline
-vim.g.indent_blankline_char = '▏'
+vim.g.indent_blankline_char = '⏐'
 vim.g.indent_blankline_space_char = ' '
 vim.g.indent_blankline_filetype_exclude = { 'help', 'packer' }
 vim.g.indent_blankline_buftype_exclude = { 'terminal', 'nofile' }
 vim.g.indent_blankline_char_highlight = 'LineNr'
-vim.g.indent_blankline_show_first_indent_level = false
 vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 -- Telescope
@@ -112,6 +139,7 @@ require('nvim-treesitter.configs').setup {
       scope_incremental = "grc",
     },
   },
+
   textobjects = {
     select = {
       enable = true,
