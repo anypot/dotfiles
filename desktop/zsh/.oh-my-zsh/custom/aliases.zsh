288e6b730162db3d7e793c62a79c84fcf3c43522
Anthony 'anypot' Poinsot <anypot@posteo.net>
2023-09-10 12:43:36 +0200

    Replace exa aliases with eza

--- desktop/zsh/.oh-my-zsh/custom/aliases.zsh
+++ desktop/zsh/.oh-my-zsh/custom/aliases.zsh
@@ -8,14 +8,14 @@ alias reboot='sudo reboot'
 alias ssh='TERM=rxvt-256color ssh'
 
 # exa
-alias ls='exa'                                                         # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias lS='exa -1'                                                      # one column, just names
-alias lt='exa --tree --level=2'                                        # tree
+alias ls='eza'                                                         # ls
+alias l='eza -lbF --git'                                               # list, size, type, git
+alias ll='eza -lbGF --git'                                             # long list
+alias llm='eza -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias lS='eza -1'                                                      # one column, just names
+alias lt='eza --tree --level=2'                                        # tree
 
 # Misc
 alias weather='curl wttr.in'
