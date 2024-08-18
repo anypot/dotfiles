1de1d537e52fef016aeb4ca1e51082cb263f702e
Anthony 'anypot' Poinsot <anypot@posteo.net>
2024-08-18 16:49:48 +0200

    Update neovim plugins

--- desktop/neovim/.config/nvim/lua/config/options.lua
+++ desktop/neovim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- If the LazyVim default leader key changes...
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
 local opt = vim.opt
 
 opt.breakindent = true -- Enable break indent
