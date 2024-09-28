e91c369e31a3758a1d5259740ee75bfe8e88b140
Anthony 'anypot' Poinsot <anypot@posteo.net>
2024-09-28 04:05:57 +0200

    Replace treesitter and lsp configs with language extras

--- desktop/neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ desktop/neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,14 @@
 return {
-  { "rebelot/kanagawa.nvim" },
+  {
+    "rebelot/kanagawa.nvim",
+    opts = {
+      theme = "wave",
+      background = {
+        dark = "dragon",
+        light = "lotus",
+      },
+    },
+  },
   {
     "LazyVim/LazyVim",
     opts = {
