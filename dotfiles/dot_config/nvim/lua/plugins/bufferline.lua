631acc7f8ec2611874f82634ede96edcf89dc82c
Anthony 'anypot' Poinsot <anypot@posteo.net>
2025-12-29 23:33:35 +0100

    Update neovim plugins

--- dotfiles/dot_config/nvim/lua/plugins/bufferline.lua
+++ dotfiles/dot_config/nvim/lua/plugins/bufferline.lua
@@ -1,13 +1,10 @@
 return {
   "akinsho/bufferline.nvim",
-  opts = function(_, opts)
-    if (vim.g.colors_name or ""):find("catppuccin") then
-      opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-    end
-    opts.options = {
+  opts = {
+    options = {
       always_show_bufferline = true,
       show_buffer_close_icons = false,
       show_close_icon = false,
-    }
-  end,
+    },
+  },
 }
