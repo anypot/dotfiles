558b2b418433118579d8ac0629ab35720acbe343
Anthony 'anypot' Poinsot <anypot@posteo.net>
2026-02-28 05:17:08 +0100

    Improve os config

--- dotfiles/dot_config/nvim/lua/plugins/sidekick.lua
+++ dotfiles/dot_config/nvim/lua/plugins/sidekick.lua
@@ -1,3 +1,26 @@
+local function aider_cmd()
+  local workdir = vim.fn.getcwd()
+  return {
+    "sudo",
+    "docker",
+    "run",
+    "--interactive",
+    "--tty",
+    "--rm",
+    "--network",
+    "host",
+    "--volume",
+    vim.fn.expand("~/.aider.conf.yml") .. ":/app/.aider.conf.yml",
+    "--volume",
+    workdir .. ":/workspace",
+    "--workdir",
+    "/workspace",
+    "--env",
+    "OLLAMA_API_BASE=http://127.0.0.1:11434",
+    "paulgauthier/aider",
+  }
+end
+
 return {
   "folke/sidekick.nvim",
   opts = {
@@ -5,6 +28,9 @@ return {
       tools = {
         kilocode = {
           cmd = { "kilo" },
+        },
+        aider = {
+          cmd = aider_cmd(),
         },
       },
     },
